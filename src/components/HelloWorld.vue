5abc543d2dacf3093622fa827e83e750ff035b73
Jonathan Orlen <pellondou.jo@gmail.com>
2021-01-14 19:45:32 +0700

    add first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <section class="vote">
+      <h3 class="give-your-vote">
+        Silahkan Berikan Penilaian anda terhadap pelayanan anda
+      </h3>
+      <div class="emoticons">
+
+      </div>
+    </section>
+    <section class="finish">
+      Terimakasih telah memberi penilaian anda
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container{
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  
+  flex-wrap: wrap;
+  text-align: center;
+}
+
+section{
+  width: 100%;
+}
+</style>
